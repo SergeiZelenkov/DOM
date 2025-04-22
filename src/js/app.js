25ac8de9d5d35f6b2ae1d715c9555e7d81762719
Sergei Zelenkov <sz.zelenkov@gmx.de>
2025-04-22 21:06:01 +0200

    html, logic

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,32 @@
-export default function getRandomIntInclusive(min, max) {
+const body = document.querySelector("body");
+let newUl = document.createElement("ul");
+
+newUl.classList.add("ul");
+body.append(newUl);
+for (let i = 0; i < 16; i++) {
+  let newLi = document.createElement("li");
+  newLi.classList.add("li");
+  newUl.append(newLi);
+}
+const allLi = document.querySelectorAll(".li");
+
+function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-setInterval(() => {
-  const allLi = document.querySelectorAll(".li");
-  const ul = document.querySelector(".ul");
+let lastIndex = -1;
 
-  for (let li of allLi) {
-    if (li.classList.contains("red")) {
-      li.remove();
-      let newLi = document.createElement("li");
-      newLi.classList.add("li");
-      ul.appendChild(newLi);
-    }
-  }
-  const updatedLi = document.querySelectorAll(".li");
-  if (updatedLi.length > 0) {
-    updatedLi[getRandomIntInclusive(0, updatedLi.length - 1)].classList.add(
-      "red"
-    );
-  }
-}, 100);
+setInterval(() => {
+  allLi.forEach((li) => li.classList.remove("red"));
+
+  let randomIndex;
+  do {
+    randomIndex = getRandomIntInclusive(0, allLi.length - 1);
+  } while (randomIndex === lastIndex);
+
+  allLi[randomIndex].classList.add("red");
+
+  lastIndex = randomIndex;
+}, 300);
